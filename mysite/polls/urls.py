b1be15ff7737a5e5f312b4e027b7ccd9d035903a
vipul43 <saifunny43@gmail.com>
2020-05-10 23:03:44 +0530

    -edited comments

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 #new views file added is to be pointed by a URLconf
-#for that add that views function to the urlpatterns
+#for that, add the views function to the urlpatterns
 #whenever a http request matching the urlpattern is made
 #this URLconf matches it to views.index function
 #thus running that specific function in that file
