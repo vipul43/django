6d4a6b616332bf2ced040e7456c960c89b784550
vipul43 <saifunny43@gmail.com>
2020-05-11 00:35:52 +0530

    - namespacing an app is a good idea - it helps to identify a url particular to this app - adding more URLconf's for added views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,8 +6,17 @@ from . import views
 #whenever a http request matching the urlpattern is made
 #this URLconf matches it to views.index function
 #thus running that specific function in that file
+
+#namespacing polls app
+app_name = 'polls'
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('time/', views.getTime, name='getTime'),
 
+    #adding more URLconf's for the functions in views
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:question_id>/vote/', views.vote, name="vote")
 ]
