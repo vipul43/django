c223a974d6e9d7239aa2aba2b98b44757190162a
vipul43 <saifunny43@gmail.com>
2020-05-10 23:07:16 +0530

    - model is most important file - added Question class and Choice class both inherited from models class - string method is used for the classes for better understanding in later parts - connected Choice class with Question class using models.ForeignKey() - So that object of Question class can access its corresponding choices and vice versa

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,32 @@
+#NOTE:
+#   WHENEVER YOU MAKE A CHANGE IN THE METHODS OF THE CLASSES
+#   AND YOU WANT TO PLAY WITH THE DJANGO DB API
+#   EXIT THE SHELL AND REENTER
+
+
 from django.db import models
 
+import datetime
+from django.utils import timezone
+
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return 'question: {}, published date: {}'.format(self.question_text, self.pub_date)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+
+class Choice(models.Model):
+    #models.ForeignKey() is used to mention the relationship between choice and question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return 'choice: {}, votes: {}'.format(self.choice_text, self.votes)
