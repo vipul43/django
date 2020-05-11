a263828a7dc47398c33b95f7bb8e19d36f2fc8dc
vipul43 <saifunny43@gmail.com>
2020-05-12 00:10:05 +0530

    - rewritten the index, detail and results function using django's generic views - votes fuunction is modified to show make voting - results function is modified to show the voting results - interaction with POST method, HttpResponseRedirect

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 #NOTE: Every function in views has a request parameter which is mandatory
 #       Also every function in views returns HttpResponse which is mandatory too
 
-from django.http import HttpResponse, Http404
-import datetime
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
+import datetime
 
 def getTime(request):
     now = datetime.datetime.now()
@@ -20,30 +18,46 @@ def getTime(request):
 #adding more views
 from polls.models import Question
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(id=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    return HttpResponse("You're looking at results of question %s." % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on the question %s." % question_id)
-
-
-#adding more functional views
-
-
 from django.template import loader
 
-def index(request):
-    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_questions_list': latest_questions_list
-    }
-    return HttpResponse(template.render(context, request))
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+from django.urls import reverse
+
+#NOTE: Always return an HttpResponseRedirect after successfully dealing
+#       with POST data. This prevents data from being posted twice if a
+#       user hits the Back button.
+
+#this vote function is now connected with the front end detail page
+#after selecting an option in the detail page form
+#the request is sent to this backend vote function using POST method
+#then depending on the choice selected in the detail page this function responds
+#and loads either the results page or detail page again
+def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError or Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
