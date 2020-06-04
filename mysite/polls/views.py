0f7907aa84b1012c0949ff112995696dfe12cc29
vipul43 <saifunny43@gmail.com>
2020-06-04 14:18:36 +0530

    - modified the IndexView and DetailView to not show future published questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,16 +22,28 @@ from django.template import loader
 
 from django.views import generic
 
+from django.utils import timezone
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_questions_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
