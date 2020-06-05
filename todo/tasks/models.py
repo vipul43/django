f7ec42407a4509dd512560c7901f66dfb81acbe3
vipul43 <saifunny43@gmail.com>
2020-06-05 16:16:09 +0530

    - task model with two fields title and complete

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    """
+        class to incorporate tasks
+    """
+    title = models.CharField(max_length=250)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
