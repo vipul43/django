051eec0bddd1892218688bb9dd8eabb254008502
vipul43 <saifunny43@gmail.com>
2021-01-10 01:18:30 +0530

    - routing and consumer setup done(similar to urls and views) - complex url pattern matching is used for routing to chatroom - base line app done

--- simplyChat/simplyChat/settings.py
+++ simplyChat/simplyChat/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ASGI_APPLICATION = "simplyChat.routing.application"
