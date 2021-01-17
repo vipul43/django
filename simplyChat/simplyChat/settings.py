a4190399868306e3323ac53d6b4ff93ece93d7c9
vipul43 <saifunny43@gmail.com>
2021-01-17 20:16:01 +0530

    ready for deployment

--- simplyChat/simplyChat/settings.py
+++ simplyChat/simplyChat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
